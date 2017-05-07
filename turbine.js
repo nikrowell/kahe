ba177959bbdbd4f0577a211b12541f91de3f90ef
Nik Rowell <nik@nikrowell.com>
2017-05-07 15:29:10 -0700

    0.3.0

--- turbine.js
+++ turbine.js
@@ -1,4 +1,4 @@
-/*! @nikrowell/turbine 0.2.4 */
+/*! @nikrowell/turbine 0.3.0 */
 (function (global, factory) {
     typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
     typeof define === 'function' && define.amd ? define(factory) :
