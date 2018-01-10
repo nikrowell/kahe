b9412e1bd2e687fb765f1e707774bdbc292d4c67
Nik Rowell <nik@nikrowell.com>
2018-01-10 13:04:20 -0700

    0.7.1

--- kahe.js
+++ kahe.js
@@ -1,4 +1,4 @@
-/*! kahe 0.7.0 */
+/*! kahe 0.7.1 */
 (function (global, factory) {
     typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
     typeof define === 'function' && define.amd ? define(factory) :
